e8b5fd2e14049957dce390d7a60972f4fa83bfa3
akhilkhandelwal13 <61618004+akhilkhandelwal13@users.noreply.github.com>
2020-03-14 13:55:15 +0530

    Update App.java

--- src/main/java/com/test/HelloWorldMaven/App.java
+++ src/main/java/com/test/HelloWorldMaven/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! first code change" );
+        System.out.println( "Hello World! First code change" );
     }
 }
 
